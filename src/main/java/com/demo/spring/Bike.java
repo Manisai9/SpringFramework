6305426d0f011484dae131c010d336c0779d8f64
Manisai Devasani <manisai@nbos.vinisoft.com>
2021-09-17 13:12:53 +0530

    By using annotations

--- src/main/java/com/demo/spring/Bike.java
+++ src/main/java/com/demo/spring/Bike.java
@@ -1,9 +1,12 @@
 package com.demo.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Bike implements Vechile {
 	
 	public void drive() {
-		System.out.println("New car");
+		System.out.println("New bike");
 	}
 
 }
